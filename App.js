c74f7b081d375ffcca32b359fbd2843b6aa01e20
nishio <>
2018-09-17 15:36:37 +0900

    reactNavigatinでフッターを作成

--- App.js
+++ App.js
@@ -10,13 +10,13 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
 
-import {RootNavigator} from './src/AppNavigator'
+import {RootNavigator, MyFooter} from './src/AppNavigator'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <RootNavigator />
+      <MyFooter />
     );
   }
 }
--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
 
 import Top from './Top';
 import Create from './Create';
@@ -9,5 +11,39 @@ const RootNavigator = createStackNavigator({
   Create: { screen: Create },
 });
 
+const MyFooter = createBottomTabNavigator(
+    {
+        Top: { screen: Top },
+        Create: { screen: Create },
+    },
+    {
+        initialRouteName: 'Top',
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+              const { routeName } = navigation.state;
+              let iconName;
+              if (routeName === 'Home') {
+                iconName = 'apps';
+              } else if (routeName === 'Settings') {
+                iconName = 'camera';
+              }
+      
+              // You can return any component that you like here! We usually use an
+              // icon component from react-native-vector-icons
+            //   return <Ionicons name={iconName} size={25} color={tintColor} />;
+            //   return <Icon name={iconName} size={25} color={tintColor}/>
+              return <Text>aaa</Text>
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+            // activeBackgroundColor: '#5ab4bd',
+            // inactiveBackgroundColor: '#ffffff',
+            // style: { borderTopWidth: 2, borderTopColor: '#5ab4bd' },
+        }
+    }
+);
 
-export { RootNavigator };
+
+export { RootNavigator, MyFooter };
