59b956a8be03ceb75249b0227c5de18d981bac0c
nishio <>
2018-11-13 00:08:55 +0900

    必要人数を入力用変数定義

--- src/components/CreateSchedule/SettingDate.js
+++ src/components/CreateSchedule/SettingDate.js
@@ -7,9 +7,18 @@ export default class SettingDate extends Component {
   constructor(props) {
     super(props);
     // console.warn(POST_SETTING)
+
+    // tmpRequiredManNumを全て0に設定
+    let tmpRequiredManNum = {}
+    for (let i in POST_SETTING.workFormList) {
+      let name = POST_SETTING.workFormList[i].name
+      tmpRequiredManNum[name] = 0
+    }
     this.state = {
       markedDates: {},
       selectedDay: "",
+      requiredManNum: {}, // それぞれの日付で必要な人数を格納
+      tmpRequiredManNum: tmpRequiredManNum, // 必要な人数を使い回せるように人数を記録しておく
       workFormList: POST_SETTING.workFormList,
     };
   }
@@ -21,12 +30,16 @@ export default class SettingDate extends Component {
   onDayPress(day) {
     let markedDates = this.state.markedDates;
     let selectedDay = this.state.selectedDay;
-    if (day.dateString in markedDates) {
-      delete markedDates[day.dateString]
+    let requiredManNum = this.state.requiredManNum;
+    let day_str = day.dateString
+    if (day_str in markedDates) {
+      delete markedDates[day_str]
       selectedDay = ""
+      delete requiredManNum[day_str]
     } else {
-      markedDates[day.dateString] = {selected: true, selectedDotColor: 'orange'}
-      selectedDay = day.dateString;
+      markedDates[day_str] = {selected: true, selectedDotColor: 'orange'}
+      selectedDay = day_str;
+      requiredManNum[day_str] = this.state.tmpRequiredManNum
     }
 
     // オブジェクトのディープコピーの為にシリアライズしてからデシリアライズ（ディープコピーしないと画面描画されない）
@@ -38,16 +51,23 @@ export default class SettingDate extends Component {
     this.setState({
       selectedDay: selectedDay
     })
+
+    this.setState({
+      requiredManNum: requiredManNum
+    })
   }
 
   render() {
     const selectedDay = this.state.selectedDay;
+    let requiredManNum = this.state.requiredManNum;
     let daySetting;
 
     if (selectedDay) {
       daySetting =
         <View>
-          <Text>{this.state.selectedDay}</Text>
+          <Text>{selectedDay}</Text>
+          {/* 必要人数の表示 */}
+          <Text>{JSON.stringify(requiredManNum[selectedDay])}</Text>
           {/* 勤務形態のリスト表示 */}
           {/* <Text>{JSON.stringify(this.state.workFormList)}</Text> */}
           <List containerStyle={{marginBottom: 20}}>
@@ -59,7 +79,7 @@ export default class SettingDate extends Component {
                   leftIcon={{ name: 'remove-circle-outline', type: 'MaterialIcons', style: {color: 'blue'} }}
                   rightIcon={{ name: 'add-circle-outline', type: 'MaterialIcons', style: {color: 'red'} }}
                   // rightTitle='delete'
-                  badge={{ value: 3, textStyle: { color: 'orange' }, containerStyle: { marginRight: 10 } }}
+                  badge={{ value: requiredManNum[selectedDay][l.name], textStyle: { color: 'orange' }, containerStyle: { marginRight: 10 } }}
                 />
               ))
             }
